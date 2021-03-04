b98d733596eae70cdcc7dba9e2ccc2a1a27f1aba
PoberzhetsAndriy <78019769+PoberzhetsAndriy@users.noreply.github.com>
2021-03-04 10:05:48 +0200

    1.3.14 v2
    Poberezhets Andriy
    коли відправляв перший раз код був не збережений повністю

---
+++ 1.3.14.cpp
@@ -0,0 +1,55 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double to2(int a) {
+    int counter=0;
+    long long int n = 0;
+    int k,m = 0;
+    while(a) {
+        k = a % 2;
+
+        a = a / 2;
+        n += k * pow(10, m);
+        m++;
+
+    }
+    return n;
+
+}
+
+double to10(int a){
+    int sum=0;
+    int i=0;
+    while(a>0){
+        int k=a%10;
+        a = (a-a%10)/10;
+        if (k == 1){
+            sum+=pow(2,i);
+        }
+        i++;
+    }
+    return sum;
+}
+
+int main() {
+    int res, i, c, n, k, b;
+    i = 0;
+    res = 0;
+    cin >> n;
+    n = to2(n);
+    c=0;
+    while (n > 0) {
+        k = n % 10;
+        n = (n - n % 10) / 10;
+        c++;
+
+        res+=k*pow(10,i);
+        i++;
+    }
+    b=pow(10,c-1);
+    res = res%b*10+k;
+    cout<<res;
+    return 0;
+}
